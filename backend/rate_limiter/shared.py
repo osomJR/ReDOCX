b02cae23d111675117896a71afbda588985a6305
unknown <awesomeosom10@gmail.com>
2026-06-12 23:57:49 +0100

    Updated shared dependencies

--- backend/rate_limiter/shared.py
+++ backend/rate_limiter/shared.py
@@ -744,8 +744,8 @@ ANONYMOUS_POLICY = RateLimitPolicy(
 
 AUTHENTICATED_FREE_POLICY = RateLimitPolicy(
     tier_name="authenticated_free",
-    total_limit=7,
-    heavy_limit=3,
+    total_limit=70,
+    heavy_limit=30,
 )
 
 
