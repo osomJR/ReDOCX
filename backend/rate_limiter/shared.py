beb28d2915b1726864694c9b07fb7d1256530829
unknown <awesomeosom10@gmail.com>
2026-07-19 06:29:02 +0100

    shared.py update

--- backend/rate_limiter/shared.py
+++ backend/rate_limiter/shared.py
@@ -1040,8 +1040,8 @@ ANONYMOUS_POLICY = RateLimitPolicy(
 
 AUTHENTICATED_FREE_POLICY = RateLimitPolicy(
     tier_name="authenticated_free",
-    total_limit=700,
-    heavy_limit=300,
+    total_limit=70,
+    heavy_limit=30,
 )
 
 
